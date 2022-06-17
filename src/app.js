094b9130d79c841ec1231450ff9a115f4b1317a3
Akash <akash.online98@gmail.com>
2022-06-17 15:16:33 +0530

    API create for Json Data

--- src/app.js
+++ src/app.js
@@ -4,13 +4,24 @@ const sql = require('mssql');
 let app = express();
 let PORT = 3000;
 
-app.use("/",(req,res,next)=>{
+const dbConfig = {
+    server:'localhost',
+    database:'Shoper9HO',
+    user:'sa',
+    password:'12345',
+    port:1433,
+    options: {
+        "enableArithAbort": true,
+        "encrypt":false
+    }
+};
+
+app.use((req,res,next)=>{
     let method = req.method;
     let url = req.url;
     let Parameter = req.params;
     let dt = new Date();
     let log = `Url: ${url}, Method: ${method}, Parameter: ${Parameter}, Date: ${dt}`;
-    console.log(dt);
     fs.appendFile("request_log.txt", log + "\n", err =>{
         if(err){
             console.log(err);
@@ -21,6 +32,26 @@ app.use("/",(req,res,next)=>{
 
 app.get("/",(req,res)=>{
     res.send("Get Method");
+})
+
+app.get("/api",(req,res)=>{
+    
+    var dbConn = new sql.ConnectionPool(dbConfig);
+    dbConn.connect().then(function () {
+        var request = new sql.Request(dbConn);
+        request.query("select * from ItemMaster where StockNo='8905284146777'").then(function (data) {
+            res.json(data.recordset);
+            dbConn.close();
+        }).catch(function (err) {
+            //8.
+            console.log(err);
+            dbConn.close();
+        });
+    }).catch(function (err) {
+        //9.
+        console.log(err);
+    });
+
 })
 
 app.listen(PORT,()=>{
